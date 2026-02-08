e51c572374807f8e2e7eeb051e657cb5a11a0653
Soumyadeep Mullick <soumyadeepmullick7@gmail.com>
2026-02-08 18:42:58 +0530

    Added Collision Checks to particle system

--- Assets/Scripts/Collision/Particle Collision/ParticleCollisionScript.cs
+++ Assets/Scripts/Collision/Particle Collision/ParticleCollisionScript.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 public class ParticleCollisionScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void OnParticleCollision2D(GameObject other)
+    void OnParticleTrigger()
     {
         Debug.Log("Particle Collision Detected.");
-        Destroy(other);
     }
 }
--- Assets/Scripts/Shoot/Fartoof.cs
+++ Assets/Scripts/Shoot/Fartoof.cs
@@ -1,15 +1,36 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Fartoof : MonoBehaviour
 {
     [SerializeField] ParticleSystem _particleSystem;
+    bool CollidersInRange = false;
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("LDJSLKFJSLDF");
+        Debug.Log("Entered Trigger.");
+        CollidersInRange = true;
+        Debug.Log($"{collision.gameObject.name}");
+        _particleSystem.trigger.AddCollider(collision);
         fart();
     }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        // if (!CollidersInRange)
+        // {
+        //     CollidersInRange = true;
+        //     _particleSystem.trigger.AddCollider(collision);
+        // } 
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        CollidersInRange = false;
+        _particleSystem.trigger.RemoveCollider(collision);
+    }
+
     void fart()
     {
         _particleSystem.Play(true);
-    }
+    }    
 }
